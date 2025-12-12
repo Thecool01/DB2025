98e82181cfcbbe480a9161a6130a94bd5943f5a8
Nikolay <2097nikola2097@gmail.com>
2025-12-13 00:34:01 +0500

    bonus lab

--- labs/bonus task/queries.sql
+++ labs/bonus task/queries.sql
@@ -5,7 +5,7 @@ CREATE DATABASE library_managment_system;
 -- Tables
 
 
-Authors
+-- Authors
 
 CREATE TABLE authors (
     author_id SERIAL PRIMARY KEY,
@@ -16,7 +16,7 @@ CREATE TABLE authors (
 
 
 
-Categories
+-- Categories
 
 CREATE TABLE categories (
     category_id SERIAL PRIMARY KEY,
@@ -25,7 +25,7 @@ CREATE TABLE categories (
 
 
 
-Books
+-- Books
 
 CREATE TABLE books (
     book_id SERIAL PRIMARY KEY,
@@ -40,7 +40,7 @@ CREATE TABLE books (
 
 
 
-Members
+-- Members
 
 CREATE TABLE members (
     member_id SERIAL PRIMARY KEY,
@@ -53,7 +53,7 @@ CREATE TABLE members (
 
 
 
-Librarians
+-- Librarians
 
 CREATE TABLE librarians (
     librarian_id SERIAL PRIMARY KEY,
@@ -63,7 +63,7 @@ CREATE TABLE librarians (
 
 
 
-Borrowings
+-- Borrowings
 
 CREATE TABLE borrowings (
     borrowing_id SERIAL PRIMARY KEY,
@@ -82,7 +82,7 @@ CREATE TABLE borrowings (
 -- 4. Insert Sample Data --
 
 
-Authors 
+-- Authors 
 
 INSERT INTO authors (name, country, birth_year) VALUES
 ('J.K. Rowling', 'UK', 1965),
@@ -108,7 +108,7 @@ INSERT INTO authors (name, country, birth_year) VALUES
 
 
 
-Categories 
+-- Categories 
 
 INSERT INTO categories (name) VALUES
 ('Fantasy'),
@@ -134,7 +134,7 @@ INSERT INTO categories (name) VALUES
 
 
 
-Books 
+-- Books 
 
 INSERT INTO books (title, author_id, category_id, pages, total_copies, available_copies, price) VALUES
 ('Harry Potter and the Sorcerer''s Stone', 1, 1, 320, 10, 7, 25),
@@ -160,7 +160,7 @@ INSERT INTO books (title, author_id, category_id, pages, total_copies, available
 
 
 
-Members 
+-- Members 
 
 INSERT INTO members (full_name, email, phone, age, registration_date) VALUES
 ('Ali Serik', 'ali@mail.com', '8707000001', 18, '2024-01-10'),
@@ -186,7 +186,7 @@ INSERT INTO members (full_name, email, phone, age, registration_date) VALUES
 
 
 
-Librarians 
+-- Librarians 
 
 INSERT INTO librarians (name, email) VALUES
 ('Alina Omar', 'alina.librarian@mail.com'),
@@ -212,7 +212,7 @@ INSERT INTO librarians (name, email) VALUES
 
 
 
-Borrowings 
+-- Borrowings 
 
 INSERT INTO borrowings (book_id, member_id, librarian_id, borrow_date, return_date, status) VALUES
 (1, 1, 1, '2024-02-01', NULL, 'borrowed'),
@@ -234,4 +234,286 @@ INSERT INTO borrowings (book_id, member_id, librarian_id, borrow_date, return_da
 (17, 17, 11, '2024-06-03', '2024-06-20', 'returned'),
 (18, 18, 12, '2024-06-10', NULL, 'borrowed'),
 (19, 19, 13, '2024-06-15', NULL, 'borrowed'),
-(20, 20, 14, '2024-06-20', '2024-07-02', 'returned');
+(20, 20, 14, '2024-06-20', '2024-07-02', 'returned'),
+(1, 5, 2, '2024-07-01', '2024-07-15', 'returned'),
+(1, 7, 4, '2024-08-10', NULL, 'borrowed'),
+(1, 12, 3, '2024-09-05', NULL, 'borrowed'),
+(2, 8, 6, '2024-07-20', '2024-08-01', 'returned'),
+(2, 14, 2, '2024-09-12', NULL, 'borrowed'),
+(8, 3, 5, '2024-07-03', NULL, 'borrowed'),
+(8, 9, 1, '2024-08-21', '2024-09-01', 'returned'),
+(8, 15, 7, '2024-09-10', NULL, 'borrowed'),
+(10, 11, 4, '2024-08-02', NULL, 'borrowed'),
+(10, 6, 8, '2024-08-19', '2024-09-01', 'returned');
+
+
+
+-- 5. Create Indexes --
+
+
+-- Simple B-tree index
+
+CREATE INDEX idx_books_title
+ON books(title);
+
+-- USING 
+SELECT * FROM books WHERE title = 'The Hobbit';
+
+
+-- Composite (multi-column) index
+
+CREATE INDEX idx_borrowinds_book_member
+ON borrowings(book_id, member_id);
+
+-- USING WHEN
+SELECT * FROM borrowings
+WHERE book_id = 5 AND member_id = 3;
+
+-- Unique Index
+
+CREATE UNIQUE INDEX idx_members_email
+ON members(email);
+
+-- Functional Index
+
+CREATE INDEX idx_members_registration_year
+ON members (DATE_PART('year', registration_date));
+
+-- USING WHEN
+SELECT * FROM members
+WHERE DATE_PART('year', registration_date) = 2024;
+
+
+-- Partial Index
+
+CREATE INDEX idx_borrowings_active
+ON borrowings(borrow_date)
+WHERE status = 'borrowed';
+
+-- USING WHEN
+SELECT * FROM borrowings WHERE status = 'borrowed';
+
+
+-- 1) INNER JOIN -- A list of all search results with the title of the book and the name of the reader
+SELECT borrowing_id,
+       books.title,
+       members.full_name,
+       librarians.name AS librarian_name,
+       borrow_date, status FROM borrowings
+JOIN books ON borrowings.book_id = books.book_id
+JOIN members ON borrowings.member_id = members.member_id
+JOIN librarians ON librarians.librarian_id = borrowings.librarian_id
+
+-- 2) INNER JOIN -- Find the most popular books (by the number of loans)
+SELECT books.title,
+       count(borrowing_id) AS total_borrowings FROM borrowings
+INNER JOIN books ON books.book_id = borrowings.book_id
+GROUP BY title
+ORDER BY total_borrowings DESC 
+LIMIT 5;
+-- Uses index: idx_borrowings_book_member.
+
+
+-- 3) LEFT JOIN
+SELECT
+    categories.name AS category,
+    COUNT(books.book_id) AS book_count
+FROM categories
+LEFT JOIN books ON categories.category_id = books.category_id
+GROUP BY name
+ORDER BY book_count DESC;
+-- Even categories without books will appear.
+
+
+-- 4) LEFT JOIN
+SELECT title
+FROM books
+LEFT JOIN borrowings ON books.book_id = borrowings.book_id
+WHERE borrowings.borrowing_id IS NULL;
+-- Find books that no one took
+
+
+-- 5) GROUP BY + aggregates -- number of readers by age group
+SELECT 
+    age,
+    COUNT(member_id) AS total_members
+FROM members
+GROUP BY age
+ORDER BY total_members DESC;
+
+-- 6) WHERE
+SELECT title, categories.name AS category_name
+FROM books
+JOIN categories ON books.category_id = categories.category_id
+WHERE categories.name = 'Fantasy';
+
+-- 7) WHERE
+SELECT *
+FROM books
+WHERE price > (SELECT AVG(price) FROM books);
+
+
+-- 8) Subquery in FROM: TOP-3 readers by the number of books borrowed
+SELECT members.full_name, S.borrow_count
+FROM (
+    SELECT member_id, count(*) AS borrow_count
+    FROM borrowings
+    GROUP BY member_id
+    ORDER BY borrow_count DESC
+    LIMIT 3
+     ) AS S
+JOIN members ON S.member_id = members.member_id;
+
+-- 9) ORDER BY + LIMIT: 10 last results
+
+SELECT
+    borrowing_id,
+    books.title,
+    members.full_name,
+    borrow_date
+FROM borrowings
+INNER JOIN books ON borrowings.book_id = books.book_id
+INNER JOIN members ON borrowings.member_id = members.member_id
+ORDER BY borrow_date DESC
+LIMIT 10;
+
+-- 10) LOWER()
+SELECT *
+FROM books
+WHERE LOWER(title) = 'the hobbit';
+
+--  Uses an index: idx_books_lower_title
+
+-- 11) WHERE -- Find all overdue issuances
+
+SELECT
+    borrowing_id,
+    books.title,
+    members.full_name,
+    borrow_date
+FROM borrowings
+INNER JOIN books ON books.book_id = borrowings.book_id
+INNER JOIN members ON members.member_id = borrowings.member_id
+WHERE status = 'overdue';
+
+-- 12) Using partial index
+
+SELECT *
+FROM borrowings
+WHERE status = 'borrowed'
+ORDER BY borrow_date DESC;
+
+-- Index : idx_borrowings_active ON borrowings(borrow_date) WHERE status='borrowed'
+
+
+-- 7. Roles and Users --
+
+-- ADMIN 
+CREATE ROLE admin;
+
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO admin;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO admin;
+
+-- MANAGER
+CREATE ROLE manager;
+
+GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA public TO manager;
+
+-- VIEWER 
+CREATE ROLE viewer;
+
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO viewer;
+
+-- Librarian
+CREATE USER librarian_user WITH PASSWORD 'password123';
+GRANT manager TO librarian_user;
+
+-- Student
+CREATE USER student_user WITH PASSWORD 'password123';
+GRANT viewer TO student_user;
+
+-- Admin
+CREATE USER admin_user WITH PASSWORD 'password123';
+GRANT admin TO admin_user;
+
+
+-- 8. Additional Component
+
+-- TRANSACTION
+
+DO $$
+DECLARE
+    v_available INT;
+BEGIN
+    -- Check the number of available copies for the selected book
+    SELECT available_copies INTO v_available
+    FROM books
+    WHERE book_id = 8
+    FOR UPDATE;  -- lock this row to prevent concurrent changes
+
+    -- IF ELSE logic to decide whether the book can be issued
+    IF v_available > 0 THEN
+
+        -- Insert a new borrowing record
+        INSERT INTO borrowings (book_id, member_id, librarian_id, borrow_date, status)
+        VALUES (8, 5, 2, CURRENT_DATE, 'borrowed');
+
+        -- Reduce the available copies count by 1
+        UPDATE books
+        SET available_copies = available_copies - 1
+        WHERE book_id = 8;
+
+        -- Show confirmation message
+        RAISE NOTICE 'Book successfully issued. Remaining copies: %', v_available - 1;
+
+    ELSE
+
+        -- If no copies are available, cancel the operation
+        RAISE NOTICE 'Error: No available copies of this book. Operation canceled.';
+
+        -- To simulate ROLLBACK inside DO block:
+        -- Raise an exception so the whole block is rolled back
+        RAISE EXCEPTION 'Not enough copies';
+
+    END IF;
+
+END $$;
+
+-- 9. Data Analysis -- 
+
+-- 1. Top 3 most popular books (by number of borrowings)
+
+SELECT 
+    books.title,
+    COUNT(borrowing_id) AS total_borrowed
+FROM borrowings
+JOIN books ON borrowings.book_id = books.book_id
+GROUP BY books.title
+ORDER BY total_borrowed DESC
+LIMIT 3;
+
+-- 2. Least Used Books (Taken 0 Times)
+
+SELECT 
+    books.title
+FROM books
+LEFT JOIN borrowings ON books.book_id = borrowings.book_id
+WHERE borrowings.borrowing_id IS NULL;
+
+
+-- 3. Total Library Income
+SELECT 
+    SUM(books.price) AS total_revenue
+FROM borrowings
+JOIN books ON borrowings.book_id = books.book_id;
+
+-- 4. Top 3 Most Active Readers
+SELECT 
+    members.full_name,
+    COUNT(b.borrowing_id) AS borrow_count
+FROM borrowings b
+JOIN members ON members.member_id = b.member_id
+GROUP BY members.full_name
+ORDER BY borrow_count DESC
+LIMIT 3;
